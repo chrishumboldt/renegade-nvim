d0b262d6aef0148cd415ec9936ff01d2072588c2
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-03-31 15:52:19 +0800

    Update lspsaga.lua

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -2,7 +2,11 @@ return {
   "glepnir/lspsaga.nvim",
   event = "LspAttach",
   config = function()
-    require("lspsaga").setup({})
+    require("lspsaga").setup({
+      ui = {
+        code_action = ""
+      }
+    })
   end,
   dependencies = {
     {"nvim-tree/nvim-web-devicons"},

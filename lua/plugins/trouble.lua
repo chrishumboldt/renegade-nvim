ee196f4e9ef9d4fc9125a0a6941bd7eb11b4676d
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-01 22:51:18 +0800

    Update trouble.lua

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,5 +2,25 @@ return {
   "folke/trouble.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons"
-  }
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+      {silent = true, noremap = true, desc = "Toggle trouble"}
+    )
+    vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+      {silent = true, noremap = true, desc = "Trouble workspace diagnostics"}
+    )
+    vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+      {silent = true, noremap = true, desc = "Trouble document diagnostics"}
+    )
+    vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+      {silent = true, noremap = true, desc = "Trouble locklist"}
+    )
+    vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+      {silent = true, noremap = true, desc = "Trouble quickfix"}
+    )
+    vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+      {silent = true, noremap = true, desc = "Trouble LSP references"}
+    )
+  end
 }

ed41ba3a7b9649cd48a28b357f1042d7ae9e5a31
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-01 23:23:57 +0800

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,15 +8,6 @@ return {
     config = function()
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
-
-      -- See `:help telescope.builtin`
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find recently opened files' })
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find file' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find help' })
-      vim.keymap.set("n", "<leader>fw", builtin.current_buffer_fuzzy_find, { desc = "Find word in current file" })
-      vim.keymap.set('n', '<leader>fW', builtin.live_grep, { desc = 'Find word in project' })
-      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Find in diagnostics' })
     end,
   },
   {

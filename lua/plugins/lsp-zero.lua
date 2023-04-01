ff7698e7970bce434fa6af1196206fd68c5338a7
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-01 18:26:49 +0800

    Create lsp-zero.lua

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,23 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v1.x",
+  dependencies = {
+    -- LSP Support
+    { "neovim/nvim-lspconfig" }, -- Required
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lua" },
+    { "jose-elias-alvarez/typescript.nvim" },
+
+    -- Snippets
+    { "L3MON4D3/LuaSnip" }, -- Required
+    { "rafamadriz/friendly-snippets" },
+  }
+}

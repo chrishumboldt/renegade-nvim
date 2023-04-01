6d642a82e6fecbaef2e989d624e7059541011c93
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-02 01:35:43 +0800

    Update gitsigns.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,14 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "│" },
+        topdelete = { text = "│" },
+        changedelete = { text = "│" },
+      }
+    })
   end
 }

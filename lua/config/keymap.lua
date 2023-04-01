ff81c5c090fc09abcc26e056b54c0b3b52241d04
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-01 23:57:15 +0800

    Update keymap.lua

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -3,29 +3,27 @@ if not wk_status then
   return
 end
 
-
-vim.keymap.set("n", "<leader>V", vim.cmd.Ex, { desc = "Exit file to netrw" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half a page" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half a page" })
-vim.keymap.set("n", "<leader>e", "<cmd>NeoTreeRevealToggle<cr>", { desc = "Toggle file explorer" })
+-- Strange cases.
 vim.keymap.set("n", "<leader>R", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace current word" })
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Paste from clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete and keep yank" })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left pane" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower pane" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper pane" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right pane" })
-vim.keymap.set("n", "<leader>Q", vim.cmd.qa, { desc = "Quit nvim" })
 vim.keymap.set({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 vim.keymap.set("v", "<", "<gv", { desc = "Indent less" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent more" })
-vim.keymap.set("n", "<leader>|", "<cmd>vsp<cr>", { desc = "Horizontal pane split" })
-vim.keymap.set("n", "<leader>-", "<cmd>sp<cr>", { desc = "Vertical pane split" })
-vim.keymap.set("n", "<leader>H", "<cmd>WhichKey<cr>", { desc = "Show key maps" })
-vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 
+-- Register with Which Key.
 wk.register({
+  ["<C-d>"] = { "<C-d>zz", "Scroll down half a page" },
+  ["<C-u>"] = { "<C-u>zz", "Scroll up half a page" },
+  ["<C-h>"] = { "<C-w>h", "Go to left pane" },
+  ["<C-j>"] = { "<C-w>j", "Go to lower pane" },
+  ["<C-k>"] = { "<C-w>k", "Go to upper pane" },
+  ["<C-l>"] = { "<C-w>l", "Go to right pane" },
+  ["<leader>|"] = { "<cmd>vsp<cr>", "Horizontal pane split" },
+  ["<leader>-"] = { "<cmd>sp<cr>", "Vertical pane split" },
+  ["<leader>E"] = { "<diagnostic>open_float<cr>", "Open floating diagnostic message" },
+  ["<leader>e"] = { "<cmd>NeoTreeRevealToggle<cr>", "Toggle file explorer" },
   ["<leader>f"] = {
     name = "Find",
     f = { "<cmd>Telescope find_files<cr>", "Find file" },
@@ -35,6 +33,8 @@ wk.register({
     W = { "<cmd>Telescope live_grep<cr>", "Find word in project" },
     d = { "<cmd>Telescope diagnostics<cr>", "Find in diagnostics" },
   },
+  ["<leader>H"] = { "<cmd>WhichKey<cr>", "Show key maps" },
+  ["<leader>Q"] = { "<cmd>qa<cr>", "Quit nvim" },
   ["<leader>t"] = {
     name = "Trouble diagnostics",
     t = { "<cmd>TroubleToggle<cr>", "Toggle trouble" },
@@ -43,5 +43,6 @@ wk.register({
     k = { "<cmd>TroubleToggle loclist<cr>", "Trouble locklist" },
     q = { "<cmd>TroubleToggle quickfix<cr>", "Trouble quickfix" },
     l = { "<cmd>TroubleToggle lsp_references<cr>", "Trouble LSP references" },
-  }
+  },
+  ["<leader>V"] = { "<cmd>Ex<cr>", "Exit file to netrw" }
 })

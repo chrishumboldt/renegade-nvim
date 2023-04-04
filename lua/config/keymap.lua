daef71c90d991aa9e0124f6340322817e1fc19bc
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-04 11:04:37 +0800

    Update keymap.lua

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -34,7 +34,7 @@ wk.register({
     d = { "<cmd>Telescope diagnostics<cr>", "Find in diagnostics" },
   },
   ["<leader>H"] = { "<cmd>WhichKey<cr>", "Show key maps" },
-  ["<leader>e"] = { "<cmd>Noice<cr>", "Show messages" },
+  ["<leader>M"] = { "<cmd>Noice<cr>", "Show messages" },
   ["<leader>Q"] = { "<cmd>qa<cr>", "Quit nvim" },
   ["<leader>t"] = {
     name = "Trouble diagnostics",

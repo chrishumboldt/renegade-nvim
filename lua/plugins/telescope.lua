e0d966df53c015b50ff9824c4e4aeba9e27f71ad
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-04 10:36:14 +0800

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,31 @@ return {
     "nvim-telescope/telescope.nvim",
     version = "*",
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
     },
     config = function()
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
+
+      require('telescope').setup({
+        defaults = {
+          border = {},
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+          },
+          layout_strategy = "horizontal",
+          prompt_prefix = " ?  ",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+        },
+      })
     end,
   },
   {

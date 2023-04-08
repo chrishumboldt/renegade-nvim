4f064cf1dc69b41a16fbd32fe5e0bbde94a27ac7
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-08 09:55:29 +0800

    Update gitsigns.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     require("gitsigns").setup({
       signs = {
-        add = { text = "│" },
-        change = { text = "│" },
-        delete = { text = "│" },
-        topdelete = { text = "│" },
-        changedelete = { text = "│" },
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "▎" },
+        topdelete = { text = "▎" },
+        changedelete = { text = "▎" },
       }
     })
   end

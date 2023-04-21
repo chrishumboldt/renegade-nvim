bed95d167d9217ac81ddba7d23bfd938a26a3926
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-04-21 22:40:13 +0800

    Update toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,16 @@ return {
   "akinsho/toggleterm.nvim",
   opts = {
     direction = "float",
+    highlights = {
+      FloatBorder = {
+        guibg = "#181825",
+        guifg = "#181825"
+      },
+      NormalFloat = {
+        guibg = "#181825"
+      }
+    },
     size = 20,
   }
 }
+

b404cdf09693a54245a97ae69ee53b4ddccb05d5
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-05-05 09:28:16 +0800

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,22 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      component_separators = { left = "", right = "/" },
+      component_separators = {
+        left = "",
+        right = "/"
+      },
       globalstatus = true,
       icons_enabled = false,
       section_separators = { left = "", right = "" },
       theme = "catppuccin",
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { '%f' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
     },
   },
 }

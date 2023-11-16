bb07d6e47407daac720a9e6f24d458e15065e281
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-11-16 09:24:37 +0800

    Update indent-blankline.lua

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,5 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  config = function()
-    require("ibl").setup({
-      space_char_blankline = " ",
-      show_current_context = false,
-    })
-  end
+  main = "ibl",
+  opts = {}
 }

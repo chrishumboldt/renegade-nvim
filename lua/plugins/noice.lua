71f286019f980f2e2ebdfa77d3b5fbfc122a3c92
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-12-21 21:06:05 +0800

    Create noice.lua

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,46 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 6,
+          col = "50%",
+        },
+        border = {
+          style = "none",
+          padding = { 1, 2 },
+        },
+        size = {
+          width = 75,
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 75,
+          height = 10,
+        },
+        border = {
+          style = "none",
+          padding = { 1 , 2 },
+        },
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  }
+}

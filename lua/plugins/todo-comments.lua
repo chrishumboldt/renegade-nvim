a27d602e39087e2b0f16d781eca875c9c79d6af7
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-12-22 09:23:52 +0800

    Update todo-comments.lua

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,7 +2,11 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("todo-comments").setup()
+    require("todo-comments").setup({
+        highlight = {
+          highlight = true
+        }
+    })
   end
 }
 

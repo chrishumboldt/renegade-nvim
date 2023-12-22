2a721c06bfd2ce7dae0d52fe1f5375b71d8b3ff2
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2023-12-22 10:16:42 +0800

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
       },
       globalstatus = true,
       icons_enabled = false,
-      section_separators = { right = "", left = "" },
+      -- section_separators = { right = "", left = "" },
       theme = "catppuccin",
     },
     sections = {

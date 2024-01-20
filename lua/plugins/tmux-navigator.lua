d425b9e419815b71efa115f86dfc5cbb481816c5
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2024-01-20 20:38:02 +0800

    Create tmux-navigator.lua

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,16 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<C-Left>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-Down>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-Up>", "<cmd><TmuxNavigateUp<cr>" },
+    { "<C-Right>", "<cmd>TmuxNavigateRight<cr>" },
+  },
+}

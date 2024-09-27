ced637572f1f009d27900e1980b4b8145ac4090c
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2024-09-27 10:52:38 +0800

    Create nvim-ufo.lua

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,16 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = "kevinhwang91/promise-async",
+  config = function()
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+
+    require("ufo").setup({
+      provider_selector = function()
+        return { "lsp", "indent" }
+      end,
+    })
+  end,
+}

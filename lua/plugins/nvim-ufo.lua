43c7698b392696477c547bd95a36886ec3f26328
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2024-09-30 09:09:07 +0800

    Update nvim-ufo.lua

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,7 +2,7 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = "kevinhwang91/promise-async",
   config = function()
-    vim.o.foldcolumn = "1"
+    vim.o.foldcolumn = "0"
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true

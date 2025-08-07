91625a1bdbb627255f8175c77c6d30e84a73e7e8
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2025-08-07 18:22:35 +0800

    Delete lua directory

--- lua/plugins/smart-splits.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mrjones2014/smart-splits.nvim",
-  lazy = false,
-  config = function()
-    require("smart-splits").setup({
-      at_edge = "stop",
-    })
-  end
-}
--- lua/plugins/vim-svelte.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "evanleck/vim-svelte",
-  dependencies = {
-    "othree/html5.vim",
-    "pangloss/vim-javascript"
-  }
-}

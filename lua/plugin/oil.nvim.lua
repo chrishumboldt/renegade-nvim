b817154fde70e080eec5f3885e578d503edbb71b
Chris Humboldt <chrishumboldt@users.noreply.github.com>
2025-08-08 11:27:41 +0800

    Update oil.nvim.lua

--- lua/plugin/oil.nvim.lua
+++ lua/plugin/oil.nvim.lua
@@ -1,15 +1,12 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      delete_to_trash = true,
-      skip_confirm_for_simple_edits = true,
-      keymaps = {
-        ["<C-h>"] = "actions.toggle_hidden"
-      }
-    })
-  end
+  opts = {
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    keymaps = {
+      ["<C-h>"] = "actions.toggle_hidden"
+    }
+  },
 }
 
